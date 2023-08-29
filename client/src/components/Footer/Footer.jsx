21fa16a3c8ef194b3d1ba0bc4c1948528946ae55
Ashutosh Maurya <mauryaashutosh666@gmail.com>
2023-08-29 22:53:05 +0530

    Done

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <div className="f-wrapper">
+      <div className="paddings innerWidth flexCenter f-container">
+        {/* left side */}
+        <div className="flexColStart f-left">
+          <img src="./logo2.png" alt="" width={120} />
+          <span className="secondaryText">
+            Our vision is to make all people <br />
+            the best place to live for them.
+          </span>
+        </div>
+
+        <div className="flexColStart f-right">
+          <span className="primaryText">Location</span>
+          <span className="secondaryText">RKGIT, 5No Raj Nagar, Ghaziabad (201003)</span>
+          <div className="flexCenter f-menu">
+            <a href="/properties" alt=" " ><span>Property</span></a>
+            <span>Services</span>
+            <span>Product</span>
+            <span>About Us</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
